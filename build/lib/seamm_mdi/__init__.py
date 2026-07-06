01957bcb550e790d585b66b9696ec45bd10683de
Paul Saxe <paul@thesaxes.net>
2026-07-06 12:54:28 -0400

    Stop tracking build artifacts (build/, egg-info, __pycache__)

--- build/lib/seamm_mdi/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""seamm_mdi -- a reusable MDI driver facility for SEAMM."""
-
-from .mdi_engine import MDIEngine  # noqa: F401
-
-from ._version import __version__  # noqa: F401
